f4d9337f1be4258a3b9b5f49ca881c14df781313
Marcin Kruszyński <mkruszynski@soldevelo.com>
2017-05-05 14:23:14 +0200

    #26: Created DHIS dataStore

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+${IL_HOST=localhost}
+
+export IL_HOST
+
+$CATALINA_HOME/bin/catalina.sh run &
+
+sleep 30
+
+curl "$IL_HOST:9000/api/dataStore/CSD-Loader-Last-Export/DHIS" -X POST -H "Content-Type: application/json" -d "{\"Facility Registry\":\"Sample Value\"}" -u admin:district -v
+
+tail -F `find $CATALINA_HOME/logs/ -name "catalina*"`
