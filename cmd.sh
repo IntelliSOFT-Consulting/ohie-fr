45e09e3d83c8c595c0cec9edff5fd122f08b1ce5
Arkadiusz Lalo <alalo@soldevelo.com>
2018-05-30 18:01:48 +0200

    ITOS-64: Fixed issue with wrong admin password and docker-compose

--- cmd.sh
+++ cmd.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 : ${SERVER_ADDRESS=localhost}
+: ${DHIS_ADMIN_PASSWORD=district}
 
 export SERVER_ADDRESS
+export DHIS_ADMIN_PASSWORD
 
 $CATALINA_HOME/bin/catalina.sh run &
 
 sleep 60
 
-curl "$SERVER_ADDRESS:9000/api/dataStore/CSD-Loader-Last-Export/DHIS" -X POST -H "Content-Type: application/json" -d "{\"Facility Registry\":\"Sample Value\"}" -u admin:district -v
+curl "$SERVER_ADDRESS:9000/api/dataStore/CSD-Loader-Last-Export/DHIS" -X POST -H "Content-Type: application/json" -d "{\"Facility Registry\":\"Sample Value\"}" -u admin:${DHIS_ADMIN_PASSWORD} -v
 
 tail -F `find $CATALINA_HOME/logs/ -name "catalina*"`
