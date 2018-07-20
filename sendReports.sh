502e9f962c7006864d7223bb7759e2566c156bf2
Karol Cissewski <kcissewski@soldevelo.com>
2018-07-20 11:42:47 +0200

    ITOS-133: Ability to send reports without period_iso

--- sendReports.sh
+++ sendReports.sh
@@ -8,6 +8,7 @@ export MYSQL_ROOT_PASSWORD
 
 USERNAME=admin;
 POST_TEMPLATE=$(cat /postTemplate.json)
+POST_TEMPLATE_NO_PERIODS=$(cat /postTemplateNoPeriods.json)
 
 processFile() {
    REPORT_CONTENT=$(envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`"  < $1)
@@ -20,8 +21,29 @@ processFile() {
    "http://localhost:8080/api/reports" -d "$(echo $POST_CONTENT)"
 }
 
+processFileNoPeriods() {
+   REPORT_CONTENT=$(envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`"  < $1)
+   REPORT_CONTENT="${REPORT_CONTENT//\"/\\\"}"
+   REPORT_NAME=$(echo $2 | sed 's/\([A-Z][^A-Z]\)/ \1/g')
+
+   POST_CONTENT=$(printf "$POST_TEMPLATE_NO_PERIODS" "$REPORT_NAME" "$REPORT_CONTENT")
+
+   curl -X POST -H "Content-Type: application/json" -u "$USERNAME:$DHIS_ADMIN_PASSWORD" \
+   "http://localhost:8080/api/reports" -d "$(echo $POST_CONTENT)"
+}
+
 for file in /reports/*; do
   filename=$(basename "$file")
   fname="${filename%.*}"
+  if [[ -f $file ]]; then
   processFile $file $fname
+  fi
+done
+
+for file in /reports/noPeriods/*; do
+  filename=$(basename "$file")
+  fname="${filename%.*}"
+  if [[ -f $file ]]; then
+  processFileNoPeriods $file $fname
+  fi
 done
